ef5631aac3db72eb7d9d4144d47fefbd22c87bcf
Ice_dera3 <hei.sedera@gmail.com>
2024-09-06 21:43:11 +0300

    fix: package.json

--- patrimoine-UI/vite.config.js
+++ patrimoine-UI/vite.config.js
@@ -3,13 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
+  build: {
+    outDir: 'dist', // Assure-toi que le dossier de build est correct
   },
 });
