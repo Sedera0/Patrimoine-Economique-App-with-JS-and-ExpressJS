17025eb3065610b7460e6cef4d3ec0077f5baf2d
Ice_dera3 <hei.sedera@gmail.com>
2024-09-06 21:46:43 +0300

    fix: package.json

--- patrimoine-UI/vite.config.js
+++ patrimoine-UI/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Assure-toi que le dossier de build est correct
+    outDir: 'dist',
   },
 });
